1c28313955b7577637cebe1e2b6776cc327fc0a7
Darrian Bagley <dbagle14@gmail.com>
2022-03-26 08:54:40 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+class Scooter {
+    constructor(year, color, model) {
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+}
+
+class Driver {
+    constructor(name, age, experience) {
+        this.name = name;
+        this.age = age;
+        this.experience = experience;
+    }
+}
+
+class PickupLocation {
+    constructor(address, city) {
+        this.city = city;
+        this.address = address;
+    }
+}
